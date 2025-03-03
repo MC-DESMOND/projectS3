baee3add66baa51be8972b9e0be1c58a6e77b993
GR-NADE <ikenuavictor@gmail.com>
2025-03-03 15:09:57 +0100

    createandsuccess

--- Okechukwu/Employee Portal/obj/Debug/net8.0/Employee Portal.AssemblyInfo.cs
+++ Okechukwu/Employee Portal/obj/Debug/net8.0/Employee Portal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1800ea1e24afe789a0fca20333ec682eff0a1b2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b07b9b16222090d13d6fe8c85d965f9ee86b5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
