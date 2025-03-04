1403d723cf006c187aa9e6c2b85e9d7f6f906ef2
MC_DESMOND <macdesmonddikeibeh@gmail.com>
2025-03-04 13:47:02 +0100

    ojohgh

--- Main/obj/Debug/net8.0/Main.AssemblyInfo.cs
+++ Main/obj/Debug/net8.0/Main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa6dc8d017bb553243fe1b991f4ab1d2433fb56f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0db2bda23c0686f0028271053962590a3dc4c22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
